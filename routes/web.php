29ce71e87b4ffc3475914012bd3c9e73733fabca
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-07-24 21:08:06 +0700

    ^ redirect after login

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () 
+{   
+    if (Auth::user()) {
+        return redirect()->route('home');
+    }
+    return view('auth/login');
 });
 
 Route::get('/token', function () {
