753d15d6c638e0b96a56adb9b4b519a709c343c4
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-07-24 17:50:47 +0700

    ^ add method insert, update, destroy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 use App\User;
+use Auth;
 
 class UserController extends Controller
 {
@@ -23,16 +25,88 @@ class UserController extends Controller
                 'name' => 'required|string',
                 'email' => 'required|string|unique:users',
                 'password' => 'required|string|min:5',
+                'role_id' => 'required',
             ]);
     
             if ($validator->fails()) {
                 return $validator->errors();
             }
 
-            dd($request->all());
+            $user = new User;
+            $user->role_id = $request->role_id;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            dd(encrypt($user->id));
 
         } catch (\Exception $e) {
             dd($e);
+            return abort(500);
         }
-    } 
+    }
+
+    public function update(Request $request, $user_id)
+    {   
+        try {
+
+            $data = $request->except('_method','_token','submit');
+
+            try {
+                $decrypted_id = decrypt($user_id);
+            } catch (\Exception $e) {
+                throw new \Exception($e->getMessage());
+            }
+
+            $validator = \Validator::make($data, [
+                'name' => 'required|string',
+                'email' => [
+                    'required',
+                    'string',
+                    Rule::unique('users', 'email')->ignore($decrypted_id),
+                ],
+                'role_id' => 'required',
+            ]);
+    
+            if ($validator->fails()) {
+                return $validator->errors();
+            }
+
+            $user = User::findOrFail($decrypted_id);
+
+            $user->update($data);
+
+            dd($user);
+
+        } catch (\Exception $e) {
+            dd($e);
+            if ("The payload is invalid." == $e->getMessage()) {
+                return abort(404);
+            }
+            return abort(500);
+        }
+    }
+
+    public function destroy(Request $request, $user_id)
+    {
+        try {
+            try {
+                $decrypted_id = decrypt($user_id);
+            } catch (\Exception $e) {
+                throw new \Exception($e->getMessage());
+            }
+
+            $user = User::findOrFail($decrypted_id);
+            $user->delete();
+            dd($user);
+            
+        } catch (\Exception $e) {
+            dd($e);
+            if ("The payload is invalid." == $e->getMessage()) {
+                return abort(404);
+            }
+            return abort(500);
+        }
+    }
 }
