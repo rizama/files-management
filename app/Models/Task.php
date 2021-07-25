7fd4c1130e67875874f9794ddf28aa09210a17aa
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-07-25 21:07:42 +0700

    ^ update relasi

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $guarded = [];
+    /**
+     * Get all of the files for the Task
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function files()
+    {
+        return $this->hasMany(File::class, 'task_id', 'id');
+    }
 }
