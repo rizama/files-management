fead33a45038c5b9ec9c11198a1e46cafafbb110
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-07-29 21:09:50 +0700

    + tambah table transaksi dan file public

---
+++ database/migrations/2021_07_29_134409_create_file_publics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilePublicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_publics', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('created_by');
+            $table->string('original_name');
+            $table->text('description');
+            $table->string('mime_type');
+            $table->string('new_name');
+            $table->string('path');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_publics');
+    }
+}
