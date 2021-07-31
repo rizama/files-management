41f6d63aec3a07580e1315864c80b523181f5d99
Mochamad Rizky Purnama <mochamadrizkypurnama@gmail.com>
2021-07-31 16:35:39 +0700

    route FE use auth

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,9 +1,17 @@
 <?php
 
-// Route
-Route::get('/users', 'UserController@index')->name('users.index');
-Route::get('/users/create', 'UserController@create')->name('users.create');
-Route::get('/users/edit/{user_id}', 'UserController@edit')->name('users.edit');
+use Illuminate\Support\Facades\Route;
 
-Route::get('/roles', 'RoleController@index')->name('roles.index');
-Route::get('/roles/edit/{role}', 'RoleController@edit')->name('roles.edit');
+Route::group(
+    [
+        'middleware' => 'auth'
+    ],
+    function () {
+        // Route
+        Route::get('/users', 'UserController@index')->name('users.index');
+        Route::get('/users/create', 'UserController@create')->name('users.create');
+        Route::get('/users/edit/{user_id}', 'UserController@edit')->name('users.edit');
+
+        Route::get('/roles', 'RoleController@index')->name('roles.index');
+        Route::get('/roles/edit/{role}', 'RoleController@edit')->name('roles.edit');
+});
