194e44063552d484e7a771e61a080bd112ce99a4
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-08-01 00:26:22 +0700

    + search controller

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\File;
+use App\Models\Task;
+use App\User;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        $keyword = $request->q;
+
+        // Person
+        $files = File::where('original_name', 'LIKE', "%$keyword%")
+            ->orWhereHas('user', function($query) use($keyword){
+                $query->where('name', 'LIKE', "%$keyword%");
+            })->orWhereHas('task', function($query) use($keyword){
+                $query->where('name', 'LIKE', "%$keyword%");
+            })->with('task', 'user')->orderBy('updated_at', 'desc')->get();
+        
+        $tasks = Task::where('name', 'LIKE', "%$keyword%")->with('user')->orderBy('updated_at', 'desc')->get();
+        
+        return response()->json(["files" => $files, "tasks" => $tasks], 200);
+
+    }
+}
