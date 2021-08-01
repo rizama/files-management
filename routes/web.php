17b2ed1ce30660e5e20c337307f73eb36c5cccf9
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-08-01 20:13:35 +0700

    ^ merge routes

--- routes/web.php
+++ routes/web.php
@@ -30,5 +30,38 @@ Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('dashboard');
 
-include('frontend.php');
-include('backend.php');
+Route::group(
+    [
+        'middleware' => 'auth'
+    ],
+    function () {
+        // User Route
+        Route::get('/users', 'UserController@index')->name('users.index');
+        Route::get('/users/create', 'UserController@create')->name('users.create');
+        Route::post('/users', 'UserController@store')->name('users.store');
+        Route::get('/users/edit/{user_id}', 'UserController@edit')->name('users.edit');
+        Route::put('/users/{user_id}', 'UserController@update')->name('users.update');
+        Route::get('/users/{user_id}', 'UserController@destroy')->name('users.destroy');
+
+        // Roles Route
+        Route::get('/roles', 'RoleController@index')->name('roles.index');
+        Route::get('/roles/edit/{role}', 'RoleController@edit')->name('roles.edit');
+        Route::put('/roles/{role_id}', 'RoleController@update')->name('roles.update');
+
+        // Tasks Route
+        Route::get('/tasks', 'TaskController@index')->name('tasks.index');
+        Route::get('/tasks/show/{id}', 'TaskController@show')->name('tasks.show');
+        Route::get('/tasks/create', 'TaskController@create')->name('tasks.create');
+        Route::post('/tasks', 'TaskController@store')->name('tasks.store');
+        Route::get('/tasks/edit/{id}', 'TaskController@edit')->name('tasks.edit');
+        Route::put('/tasks/{id}', 'TaskController@update')->name('tasks.update');
+        Route::get('/tasks/{id}', 'TaskController@destroy')->name('tasks.destroy');
+
+        // Files Route
+        Route::get('/files/get-local', 'FileController@get_local_file');
+        Route::get('/files/get-s3', 'FileController@get_s3_file');
+
+        //Searches
+        Route::get('/searches', 'SearchController@index')->name('searches.index');
+        Route::get('/search', 'SearchController@search')->name('do.search');        
+});
