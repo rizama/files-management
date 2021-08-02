d6d9ded7efaa75597b803ebdb6f027e641f6cb99
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-08-02 23:57:02 +0700

    ^ add status approve in task

--- database/migrations/2021_08_01_135933_add_status_approve_to_tasks_table.php
+++ database/migrations/2021_08_01_135933_add_status_approve_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddStatusApproveToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->integer('status_approve')->nullable();
+            $table->integer('status')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddStatusApproveToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('status_approve');
+            $table->dropColumn('status');
         });
     }
 }
