11116a50b01205880b7193a36f54ada23c815c24
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-08-05 19:11:30 +0700

    ^ logic count widget

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use App\User;
 use Illuminate\Http\Request;
 use Auth;
 
@@ -33,6 +35,44 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tasks = Task::with(['files' => function($q){
+            $q->where('is_default', 0)->orderBy('updated_at', 'desc');
+        }])->get();
+
+        $done = 0;
+        $waiting = 0;
+        $progress = 0;
+        foreach ($tasks as $index => $task) {
+            if ($task->status == 3) {
+                $done++;
+            } else {
+                if (count($task->files)) {
+                    if ($task->files[0]->status_approve == 2) {
+                        $waiting++;
+                    }
+                    if (($task->files[0]->status_approve == 4)) {
+                        $progress++;
+                    }
+                } else {
+                    if ($task->status == 1) {
+                        $progress++;
+                    }
+                }
+            }   
+        }
+
+        $users = User::with(['responsible_tasks', 'role'])
+            ->whereHas('role', function($query){
+                $query->where('code', '!=', 'superadmin');
+            })
+            ->get();
+        dd($users);
+
+        $res['task_total'] = count($tasks);
+        $res['task_done'] = $done;
+        $res['task_waiting'] = $waiting;
+        $res['task_progress'] = $progress;
+
+        return view('home', $res);
     }
 }
