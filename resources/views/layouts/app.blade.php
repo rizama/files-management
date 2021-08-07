f468a4d7a0ea0c1a1fbb0e098f29770429073c59
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-08-07 13:17:06 +0700

    ^ update link menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
                                 </a>
                             </li>
                             <li class="nav-main-item">
-                                <a class="nav-main-link {{ Request::is('searches*') ? 'active' : '' }}" href="{{ url('/searches') }}">
+                                <a class="nav-main-link {{ Request::is('search*') ? 'active' : '' }}" href="{{ url('/search') }}">
                                     <i class="nav-main-link-icon si si-magnifier"></i>
                                     <span class="nav-main-link-name">Pencarian</span>
                                 </a>
