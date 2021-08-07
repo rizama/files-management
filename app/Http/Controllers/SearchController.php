fe5305a494285fac93fec3491670cd6542da3f36
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-08-07 13:16:34 +0700

    ^ standar search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,8 +9,29 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $keyword = $request->q;
+        if ($keyword) {
+             // Person
+            $files = File::where('original_name', 'LIKE', "%$keyword%")->where('status_approve', 3)
+                ->orWhereHas('user', function($query) use($keyword){
+                    $query->where('name', 'LIKE', "%$keyword%");
+                })->orWhereHas('task', function($query) use($keyword){
+                    $query->where('name', 'LIKE', "%$keyword%")->where('status', 1);
+                })->with('task', 'user')->orderBy('updated_at', 'desc')->get();
+
+            $tasks = Task::with(['responsible_person', 'user'])
+                ->whereHas('responsible_person', function($q) use($keyword){
+                    $q->where('name', 'LIKE', "%$keyword%");
+                })
+                ->orWhere('name', 'LIKE', "%$keyword%")->orderBy('updated_at', 'desc')->get();
+
+            $ret['files'] = $files;
+            $ret['tasks'] = $tasks;
+            // return response()->json(["files" => $files, "tasks" => $tasks], 200);
+            return view('searches.index', $ret);
+        }
         return view('searches.index');
     }
 
@@ -18,16 +39,22 @@ class SearchController extends Controller
     {
         $keyword = $request->q;
 
+        // ->where('name', 'LIKE', "%$keyword%")
+        
         // Person
-        $files = File::where('original_name', 'LIKE', "%$keyword%")
+        $files = File::where('original_name', 'LIKE', "%$keyword%")->where('status_approve', 3)
             ->orWhereHas('user', function($query) use($keyword){
                 $query->where('name', 'LIKE', "%$keyword%");
             })->orWhereHas('task', function($query) use($keyword){
-                $query->where('name', 'LIKE', "%$keyword%");
+                $query->where('name', 'LIKE', "%$keyword%")->where('status', 1);
             })->with('task', 'user')->orderBy('updated_at', 'desc')->get();
-        
-        $tasks = Task::where('name', 'LIKE', "%$keyword%")->with('user')->orderBy('updated_at', 'desc')->get();
-        
+
+        $tasks = Task::with(['responsible_person', 'user'])
+            ->whereHas('responsible_person', function($q) use($keyword){
+                $q->where('name', 'LIKE', "%$keyword%");
+            })
+            ->orWhere('name', 'LIKE', "%$keyword%")->orderBy('updated_at', 'desc')->get();
+
         return response()->json(["files" => $files, "tasks" => $tasks], 200);
 
     }
