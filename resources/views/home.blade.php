fcfd28a70c581bc19aaa03d65944200c3d8fea86
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-08-15 00:35:32 +0700

    ^ redesign & status penugasan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
 @endsection
 <div class="row">
     <div class="col-6 col-lg-3">
-        <a class="block block-rounded block-link-shadow text-center" href="be_pages_ecom_orders.html">
+        <a class="block block-rounded block-link-shadow text-center" href="javascript:void(0)">
             <div class="block-content block-content-full">
                 <div class="font-size-h2 text-primary">{{ $task_total }}</div>
             </div>
@@ -69,71 +69,113 @@
 </div>
 
 <!-- Table Sections (.js-table-sections class is initialized in Helpers.tableToolsSections()) -->
-<div class="block block-rounded">
-    <div class="block-header">
-        <h3 class="block-title">Perkembangan Pekerjaan</h3>
+<div class="row">
+    <div class="col-md-6">
+        <div class="block block-rounded">
+            <div class="block-content">
+                <table class="js-table-sections table table-hover table-vcenter">
+                    <thead>
+                        <tr>
+                            <th style="width: 30px; border-top-width: 0;" rowspan="2"></th>
+                            <th rowspan="2" style="vertical-align: middle; border-top-width: 0;">Nama</th>
+                            <th colspan="5" class="text-center" style="border-top-width: 0;">Status Dokumen</th>
+                        </tr>
+                        <tr>
+                            <th style="width: 15%;" class="text-center">Disetujui</th>
+                            <th style="width: 15%;" class="text-center">Menunggu</th>
+                            <th style="width: 15%;" class="text-center">Ditolak</th>
+                            <th style="width: 15%;" class="text-center">Total</th>
+                        </tr>
+                    </thead>
+        
+                    @foreach ($users as $user)
+                    <tbody class="js-table-sections-header">
+                        <tr>
+                            <td class="text-center">
+                                <i class="fa fa-angle-right text-muted"></i>
+                            </td>
+                            <td class="font-w600 font-size-sm">
+                                <div class="py-1">
+                                    {{ $user->name }}
+                                </div>
+                            </td>
+                            <td class="text-center" style="font-size: 1.5em;">
+                                <span class="badge badge-pill badge-success">{{ count($user->files->where('status_approve', 3)) }}</span>
+                            </td>
+                            <td class="text-center" style="font-size: 1.5em;">
+                                <span class="badge badge-pill badge-dark">{{ count($user->files->where('status_approve', 2)) }}</span>
+                            </td>
+                            <td class="text-center" style="font-size: 1.5em;">
+                                <span class="badge badge-pill badge-danger">{{ count($user->files->where('status_approve', 4)) }}</span>
+                            </td>
+                            <td class="text-center" style="font-size: 1.5em;">
+                                <span class="badge badge-pill badge-primary">{{ count($user->files) }}</span>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody class="font-size-sm">
+                        @foreach ($user->files as $file)
+                        <tr>
+                            @php
+                                if($file->status_approve == 3){
+                                    $status = ['success', 'Disetujui'];
+                                } else if ($file->status_approve == 2) {
+                                    $status = ['dark', 'Menunggu'];
+                                } else if ($file->status_approve == 4) {
+                                    $status = ['danger', 'Ditolak'];
+                                }
+                            @endphp
+                            <td colspan="6" class="font-w600 font-size-sm"><a href="{{ route('tasks.show', encrypt($file->task->id)) }}"><span class="mr-2 badge badge-{{ $status[0] }}">{{ $status[1] }}</span> {{ $file->task->name }}</a></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    @endforeach
+                </table>
+            </div>
+        </div>
     </div>
-    <div class="block-content">
-        <table class="js-table-sections table table-hover table-vcenter">
-            <thead>
-                <tr>
-                    <th style="width: 30px;" rowspan="2"></th>
-                    <th rowspan="2" style="vertical-align: middle;">Nama</th>
-                    <th colspan="5" class="text-center">Dokumen</th>
-                </tr>
-                <tr>
-                    <th style="width: 15%;" class="text-center">Disetujui</th>
-                    <th style="width: 15%;" class="text-center">Menunggu</th>
-                    <th style="width: 15%;" class="text-center">Ditolak</th>
-                    <th style="width: 15%;" class="text-center">Total</th>
-                </tr>
-            </thead>
-            {{-- {{ dd($users[2]) }} --}}
-
-            @foreach ($users as $user)
-            <tbody class="js-table-sections-header">
-                <tr>
-                    <td class="text-center">
-                        <i class="fa fa-angle-right text-muted"></i>
-                    </td>
-                    <td class="font-w600 font-size-sm">
-                        <div class="py-1">
-                            {{ $user->name }}
-                        </div>
-                    </td>
-                    <td>
-                        <span class="badge badge-success">{{ count($user->files->where('status_approve', 3)) }}</span>
-                    </td>
-                    <td>
-                        <span class="badge badge-dark">{{ count($user->files->where('status_approve', 2)) }}</span>
-                    </td>
-                    <td>
-                        <span class="badge badge-danger">{{ count($user->files->where('status_approve', 4)) }}</span>
-                    </td>
-                    <td>
-                        <span class="badge badge-primary">{{ count($user->files) }}</span>
-                    </td>
-                </tr>
-            </tbody>
-            <tbody class="font-size-sm">
-                @foreach ($user->files as $file)
-                <tr>
-                    @php
-                        if($file->status_approve == 3){
-                            $status = ['success', 'Disetujui'];
-                        } else if ($file->status_approve == 2) {
-                            $status = ['dark', 'Menunggu'];
-                        } else if ($file->status_approve == 4) {
-                            $status = ['danger', 'Ditolak'];
-                        }
-                    @endphp
-                    <td class="text-center"><span class="badge badge-{{ $status[0] }}">{{ $status[1] }}</span></td>
-                    <td colspan="5" class="font-w600 font-size-sm"><a href="{{ route('tasks.show', encrypt($file->task->id)) }}">{{ $file->task->name }}</a></td>
-                </tr>
-                @endforeach
-            </tbody>
-            @endforeach
-        </table>
+    <div class="col-md-6">
+        <div class="block block-rounded">
+            <div class="block-content">
+                <table class="js-table-sections table table-hover table-vcenter">
+                    <thead>
+                        <tr>
+                            <th rowspan="2" style="vertical-align: middle; border-top-width: 0;">Nama</th>
+                            <th colspan="5" class="text-center" style="border-top-width: 0;">Status Penugasan</th>
+                        </tr>
+                        <tr>
+                            <th style="width: 15%;" class="text-center">Disetujui</th>
+                            <th style="width: 15%;" class="text-center">Diproses</th>
+                            <th style="width: 15%;" class="text-center">Total</th>
+                        </tr>
+                    </thead>
+        
+                    @foreach ($data_task as $name => $task)
+                    <tbody class="js-table-sections-header">
+                        <tr>
+                            <td class="font-w600 font-size-sm">
+                                <div class="py-1">
+                                    <img class="img-avatar img-avatar48 mr-1" src="{{  asset('media/avatars/avatar1.jpg')  }}" alt="">
+                                    {{ $name }}
+                                </div>
+                            </td>
+                            <td class="text-center" style="font-size: 1.5em;">
+                                <span class="badge badge-pill badge-success">{{ $task['finish'] }}</span>
+                            </td>
+                            <td class="text-center" style="font-size: 1.5em;">
+                                <span class="badge badge-pill badge-warning">{{ $task['progres'] }}</span>
+                            </td>
+                            <td class="text-center" style="font-size: 1.5em;">
+                                <span class="badge badge-pill badge-primary">{{ $task['total'] }}</span>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody class="font-size-sm">
+                    </tbody>
+                    @endforeach
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 <!-- END Table Sections -->
