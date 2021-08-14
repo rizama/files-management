61722c76faa6377dd83849560efd2120218b834c
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-08-14 16:29:59 +0700

    ^ register seeder category

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(StatusApprovesTableSeeder::class);
         $this->call(RolesTableSeeder::class);
+        $this->call(CategorySeeder::class);
     }
 }
