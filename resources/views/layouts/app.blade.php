d04a1570d5b405e7ec87d143210e4b09d21925ea
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-08-25 23:29:56 +0700

    ^ contoh pemanggilan global variable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -183,7 +183,7 @@
                     <div class="dropdown d-inline-block ml-2">
                         <button type="button" class="btn btn-sm btn-dual d-flex align-items-center" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <img class="rounded-circle" src="{{ asset('media/avatars/avatar10.jpg') }}" alt="Header Avatar" style="width: 21px;">
-                            <span class="d-none d-sm-inline-block ml-2">{{ Auth::user()->username }}</span>
+                            <span class="d-none d-sm-inline-block ml-2">{{ $notif_count }} - {{ Auth::user()->username }}</span>
                             <i class="fa fa-fw fa-angle-down d-none d-sm-inline-block ml-1 mt-1"></i>
                         </button>
                         <div class="dropdown-menu dropdown-menu-md dropdown-menu-right p-0 border-0" aria-labelledby="page-header-user-dropdown">
